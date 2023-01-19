b9b4748e8c47051ef40d9a54cbc8bfd425bf3958
Bruke <BrukeD12@gmail.com>
2023-01-19 14:31:23 -0800

    monty ten

--- file_ops.c
+++ file_ops.c
@@ -95,6 +95,7 @@ void find_func(char *opcode, char *value, int ln, int format)
 		{"sub", sub_nodes},
 		{"div", div_nodes},
 		{"mul", mul_nodes},
+		{"mod", mod_nodes},
 		{NULL, NULL}
 	};
 
--- monty.h
+++ monty.h
@@ -65,6 +65,7 @@ void nop(stack_t **stack, unsigned int line_number);
 void sub_nodes(stack_t **stack, unsigned int line_number);
 void div_nodes(stack_t **stack, unsigned int line_number);
 void mul_nodes(stack_t **stack, unsigned int line_number);
+void mod_nodes(stack_t **stack, unsigned int line_number);
 
 /*Error hanlding*/
 void err(int error_code, ...);
